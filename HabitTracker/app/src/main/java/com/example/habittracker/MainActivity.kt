f861bf4d7d682cac8f9188f3c178bc2cad6bb99d
Alexander <villafuertequispealex@gmail.com>
2026-04-28 13:54:44 -0500

    filtros

--- HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.kt
+++ HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -11,14 +11,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -28,12 +35,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.habittracker.data.Habit
 import com.example.habittracker.data.HabitDatabase
 import com.example.habittracker.data.HabitViewModel
+
+enum class FiltroHabit { TODOS, COMPLETADOS, NO_COMPLETADOS }
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -107,6 +117,16 @@ fun HabitItem(
 fun HabitTrackerApp(viewModel: HabitViewModel) {
     var input by  remember { mutableStateOf("") }
     val habits by viewModel.habits.collectAsState(initial = emptyList())
+
+    // Filtros
+    var filtroActual by remember { mutableStateOf(FiltroHabit.TODOS) }
+
+    var habitsFiltrados = when (filtroActual) {
+        FiltroHabit.TODOS -> habits
+        FiltroHabit.COMPLETADOS -> habits.filter { it.isCompletedToday }
+        FiltroHabit.NO_COMPLETADOS -> habits.filter { !it.isCompletedToday }
+    }
+
     // Cálculo derivado (recomposición inteligente)
     val completedCount = habits.count { it.isCompletedToday }
     val progress = if (habits.isNotEmpty()) { completedCount.toFloat() / habits.size
@@ -149,9 +169,16 @@ fun HabitTrackerApp(viewModel: HabitViewModel) {
                 Text("Agregar")
             }
         }
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+
+        SeccionFiltros(
+            filtroActual = filtroActual,
+            onFiltroChange = { filtroActual = it }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
         LazyColumn {
-            items(habits, key = { it.id }) { habit ->
+            items(habitsFiltrados, key = { it.id }) { habit ->
                 HabitItem(
                     habit = habit,
                     onToggle = { checked ->
@@ -162,6 +189,26 @@ fun HabitTrackerApp(viewModel: HabitViewModel) {
                     }
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun SeccionFiltros(filtroActual: FiltroHabit, onFiltroChange: (FiltroHabit) -> Unit){
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        FiltroHabit.entries.forEach { filtro ->
+            val seleccionado = filtro == filtroActual
+            FilterChip(
+                selected = seleccionado,
+                onClick = { onFiltroChange(filtro) },
+                label = { Text(filtro.name.lowercase().replaceFirstChar { it.uppercase() }.replace("_", " ")) },
+                leadingIcon = if (seleccionado) {
+                    { Icon(Icons.Default.Check, contentDescription = null, modifier = Modifier.size(18.dp) ) }
+                } else null
+            )
         }
     }
 }
