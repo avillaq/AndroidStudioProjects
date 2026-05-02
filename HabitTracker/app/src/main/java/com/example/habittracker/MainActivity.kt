82f3bfa4a806447031fe52ece59de1af5c94f5b4
Alexander <villafuertequispealex@gmail.com>
2026-05-02 01:41:59 -0500

    tema

--- HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.kt
+++ HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.habittracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.habittracker.navigation.Navigation
+import com.example.habittracker.ui.theme.HabitTrackerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,9 +17,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                val navController = rememberNavController()
-                Navigation(navController = navController)
+            HabitTrackerTheme(dynamicColor = false) {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    Navigation(navController = navController)
+                }
             }
         }
     }
